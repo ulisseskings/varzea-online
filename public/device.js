f451dfda1ad52532d7142c31fb0b1c9ddd5b0b7a
UlissesKings <47ulisses@gmail.com>
2026-02-26 18:01:14 -0300

    correcao 6

---
+++ public/device.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  localStorage.clear();
+  const desktopBtn = document.getElementById("desktopBtn");
+  const mobileBtn = document.getElementById("mobileBtn");
+
+  if (desktopBtn) {
+    desktopBtn.addEventListener("click", () => {
+      localStorage.setItem("deviceMode", "desktop");
+      window.location.href = "/lobby.html";
+    });
+  }
+
+  if (mobileBtn) {
+    mobileBtn.addEventListener("click", () => {
+      localStorage.setItem("deviceMode", "mobile");
+      window.location.href = "/lobby-mobile.html";
+    });
+  }
+
+});
