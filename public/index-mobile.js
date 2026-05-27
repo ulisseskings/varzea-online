519d481833f32159f42cd461016f908d04fedc2a
UlissesKings <47ulisses@gmail.com>
2026-05-27 20:29:11 -0300

    correcao 9

--- public/index-mobile.js
+++ public/index-mobile.js
@@ -3137,7 +3137,7 @@ function applyFirstHalfLayout(){
   if(tempo) tempo.innerText = "1º Tempo";
 
   const bg = document.getElementById("boardBg");
-  if(bg) bg.src = "https://i.imgur.com/auIBYLo.jpg?1";
+  if(bg) bg.src = "https://i.imgur.com/9Fam9LP.png";
 
   document.querySelectorAll(".anchor").forEach(anchor=>{
     if(anchor.dataset.initialLeft){
